5e90f2756c370270f77b375b1df2174b5b01ff71
Caio de Souza Lima <caio.lima@wises.com.br>
2023-08-27 22:36:36 -0300

    Adição de anotation customizada para listener e boas praticas de codigo

--- str-consumer/src/main/java/com/souza/caio/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/souza/caio/strconsumer/listeners/StrConsumerListener.java
@@ -1,7 +1,8 @@
 package com.souza.caio.strconsumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.souza.caio.strconsumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -9,8 +10,18 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListener {
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-	public void listener(String message) {
-		log.info("Receive message{}", message);
+	@StrConsumerCustomListener(groupId="group-1")
+	public void create(String message) {
+		log.info("CREATE ::: Receive message{}", message);
+	}
+	
+	@StrConsumerCustomListener(groupId="group-1")
+	public void log(String message) {
+		log.info("LOG ::: Receive message{}", message);
+	}
+	
+	@StrConsumerCustomListener(groupId="group-2")
+	public void history(String message) {
+		log.info("HISTORY", message);
 	}
 }
